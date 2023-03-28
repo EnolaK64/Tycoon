965b594b1c158adfd0e0c7fde43a51bd9822be8e
quev-le-geek <kevinvasilescu@gmail.com>
2023-03-28 21:57:28 +0200

    add code to indexR.js

---
+++ indexR.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js")
+const { TOKEN } = require("token.js")
+const { SlashCommandBuilder} = require("@discordjs/builders");
+const Client = new Dsicord.Client({
+    intents:[
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+//slash command
+const data = new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("renvoie pong")
+
+
+    //bot token
+Client.login(TOKEN)
+
+//bot connection
+Client.on("ready", () => {
+
+    //def command on all servers
+    //Client.application.commands.create(data);
+
+    //def command just in my server
+    Client.guilds.cache.get("1009915692977504348").commande.create(data)
+
+
+    console.log("bot ready");
+})
