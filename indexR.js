a32654e2b9335b3047f7fd2f7220695a09734cad
quev-le-geek <kevinvasilescu@gmail.com>
2023-03-29 23:41:51 +0200

    change /set command

--- indexR.js
+++ indexR.js
@@ -1,61 +1,81 @@
 const fs = require('fs');
 const Discord = require("discord.js")
 const { TOKEN } = require("../token.js")
-const { SlashCommandBuilder} = require("@discordjs/builders");
+const { SlashCommandBuilder } = require("@discordjs/builders");
 const Client = new Discord.Client({
-    intents:[
+    intents: [
         Discord.Intents.FLAGS.GUILDS,
         Discord.Intents.FLAGS.GUILD_MESSAGES
     ]
 });
 
 //slash command
-const data = 
+const data =
     new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("renvoie pong");
+        .setName("ping")
+        .setDescription("renvoie pong");
 
 const data2 =
     new SlashCommandBuilder()
-    .setName("bonjour")
-    .setDescription("Dire bonjour au bot (il te repond Bonjour)")
-    
-    const dataSet = new SlashCommandBuilder()
+        .setName("bonjour")
+        .setDescription("Dire bonjour au bot (il te repond Bonjour)")
+
+const dataSet = new SlashCommandBuilder()
     .setName('set')
     .setDescription('Stocke les données dans un fichier JSON')
     .addStringOption(option =>
         option.setName('input')
             .setDescription('Les données à stocker')
             .setRequired(true));
-    
-    //bot token
+
+const dataGet = new SlashCommandBuilder()
+    .setName('get')
+    .setDescription('Récupère les données du fichier JSON');
+
+//bot token
 Client.login(TOKEN)
 
 
 //detect interactions
-Client.on("interactionCreate", interaction =>{
+Client.on("interactionCreate", interaction => {
 
     //ping
-    if(interaction.isCommand()){
-        if(interaction.commandName === "ping"){
+    if (interaction.isCommand()) {
+        if (interaction.commandName === "ping") {
             interaction.reply("pong");
         }
     }
     //bonjour
-    if(interaction.isCommand()){
-        if(interaction.commandName === "bonjour"){
+    if (interaction.isCommand()) {
+        if (interaction.commandName === "bonjour") {
             interaction.reply("Bonjour à toi.");
         }
     }
 
     if (interaction.isCommand() && interaction.commandName === 'set') {
         const input = interaction.options.getString('input');
-        const data = { input: input };
-        fs.writeFileSync('bdd.json', JSON.stringify(data));
+        const userId = interaction.user.id;
+        const channelId = interaction.channel.id;
+        const data = { input: input, userId: userId, channelId: channelId };
+        let storedData = [];
+        try {
+            storedData = JSON.parse(fs.readFileSync('bdd.json'));
+        } catch (error) {
+            console.error(error);
+        }
+        storedData.push(data);
         (async () => {
             await interaction.reply("c'est set !");
         })();
     }
+
+    if (interaction.isCommand() && interaction.commandName === 'get') {
+        const data = JSON.parse(fs.readFileSync('bdd.json'));
+        (async () => {
+            await interaction.reply(`Les données stockées sont : ${data.input}`);
+        })();
+    }
+
 })
 
 //bot connection
@@ -65,6 +85,7 @@ Client.on("ready", () => {
     Client.application.commands.create(data);
     Client.application.commands.create(data2);
     Client.application.commands.create(dataSet);
+    Client.application.commands.create(dataGet);
 
     //def command just in my server
     /*console.log(Client.guilds.cache.get("1009915692977504348").commands.cache);
@@ -76,7 +97,7 @@ Client.on("ready", () => {
         command.delete()
     });*/
     //Client.guilds.cache.get("1009915692977504348").commands.create(data)
-    
+
 
 
     console.log("bot ready");
